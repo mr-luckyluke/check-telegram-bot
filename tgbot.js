5e0e06256b0e7807e359f280cfabe0951af20ea7
Lucky Luke <luckydevn16@gmail.com>
2024-06-14 08:20:13 +0200

    update tgbot

--- tgbot.js
+++ tgbot.js
@@ -1,19 +1,34 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf');
 
-const bot = new Telegraf(process.env.API_TELEGRAM_BOT_TOKEN);
+// const bot = new Telegraf(process.env.API_TELEGRAM_BOT_TOKEN);
 
-bot.start((ctx) => ctx.reply('Hello, World!'));
+// bot.start((ctx) => ctx.reply('Hello, World!'));
 
-bot.catch((err) => {
-  console.log('Error encountered:', err);
-});
+// bot.catch((err) => {
+//   console.log('Error encountered:', err);
+// });
 
-bot.launch().then(() => {
-  console.log('Bot started successfully');
-}).catch((error) => {
-  console.error('Error launching the bot:', error);
-});
+// bot.launch().then(() => {
+//   console.log('Bot started successfully');
+// }).catch((error) => {
+//   console.error('Error launching the bot:', error);
+// });
+
+const bot = new Telegraf(TelegramConfig.API_TELEGRAM_BOT_TOKEN)
+
+  bot.start((ctx) => {
+    ctx.reply(
+      `Welcome to the MegatonProbot!`,
+      Markup.inlineKeyboard([Markup.button.webApp("Play", AppConfig.bot_url)])
+    )
+  })
+
+  bot.launch().catch((error) => {
+    console.error("Failed to launch the bot:", error)
+  })
+
+  console.log("Bot is set up and running.")
 
 // Enable graceful stop
 process.once('SIGINT', () => bot.stop('SIGINT'));
